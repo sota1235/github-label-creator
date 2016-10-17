ad05ca4f5d17f30ba90c9c72257111e8fbfa7b43
sota1235 <sota1235@gmail.com>
2016-10-17 15:53:35 +0900

    modify: remove all issues

---
+++ src/issue-creater.js
@@ -0,0 +1,23 @@
+/**
+ * @fileoverview GitHub issue creater.
+ */
+
+import $ from 'jquery'
+
+// TODO: check the URL
+
+/**
+ * @description Count the issues displayed.
+ * @return {$}
+ */
+const getDisplayedIssues = () => $('.js-details-container.labels-list-item');
+
+// Remove all default labels.
+getDisplayedIssues().each((index, elm) => {
+  const deleteIcon   = $(elm).find('.labels-list-action.js-details-target');
+  const deleteButton = $(elm).find('.js-delete-label > button');
+  // Click delete icon.
+  deleteIcon.trigger('click');
+  // Click delete label button.
+  deleteButton.trigger('click');
+})
