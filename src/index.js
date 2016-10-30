f7c01318a8e94a113e0fc65bfce7441d53a7f261
sota1235 <sota1235@gmail.com>
2016-10-30 12:05:23 +0900

    modify: fix to send label data using chrome API

--- src/index.js
+++ src/index.js
@@ -4,48 +4,15 @@
 
 import domready from 'domready';
 import Constants from './constants';
-import { get, put } from './storage';
 import { createNewLabels, deleteLabels } from './label-creater';
 
 domready(() => {
-  // const issues = get('issues');
-
-  /** @var {array} labels - Label list. */
-  const labels = [
-    {
-      name: 'bug',
-      color: 'e99695',
-    },
-    {
-      name: 'design',
-      color: 'fef2c0',
-    },
-    {
-      name: 'feature',
-      color: 'c2e0c6',
-    },
-    {
-      name: 'for development',
-      color: 'f9d0c4',
-    },
-    {
-      name: 'improvement',
-      color: 'c5def5',
-    },
-    {
-      name: 'incompatible',
-      color: 'ffffff',
-    },
-    {
-      name: 'release',
-      color: 'd4c5f9',
-    },
-  ];
-
+  console.log('js loaded');
   chrome.runtime.onMessage.addListener(message => {
-    if (message === Constants.CLICK) {
+    console.log(message);
+    if (message.type === Constants.CLICK) {
       deleteLabels();
-      createNewLabels(labels);
+      createNewLabels(message.labels);
     }
   });
 });
--- src/popup.js
+++ src/popup.js
@@ -2,14 +2,19 @@
  * @fileoverview Main script for popup.html
  */
 
+import 'babel-polyfill';
 import $ from 'jquery';
 import domready from 'domready';
 import Constants from './constants';
+import { get, put } from './storage';
 
-domready(() => {
+domready(async () => {
+  const labels = await get('labels');
+
   $('button.create-labels').on('click', () => {
+    console.log(labels);
     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-      chrome.tabs.sendMessage(tabs[0].id, Constants.CLICK);
+      chrome.tabs.sendMessage(tabs[0].id, { type: Constants.CLICK, labels });
     });
   });
 });
