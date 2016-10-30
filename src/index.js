04cb98f516bfc10637aa5ddb743c8f0715c8605c
sota1235 <sota1235@gmail.com>
2016-10-30 12:32:41 +0900

    modify: remove debug code

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import Constants from './constants';
 import { createNewLabels, deleteLabels } from './label-creater';
 
 domready(() => {
-  console.log('js loaded');
   chrome.runtime.onMessage.addListener(message => {
-    console.log(message);
     if (message.type === Constants.CLICK) {
       deleteLabels();
       createNewLabels(message.labels);
--- src/popup.js
+++ src/popup.js
@@ -12,7 +12,6 @@ domready(async () => {
   const labels = await get('labels');
 
   $('button.create-labels').on('click', () => {
-    console.log(labels);
     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
       chrome.tabs.sendMessage(tabs[0].id, { type: Constants.CLICK, labels });
     });
